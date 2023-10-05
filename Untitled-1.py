6b87378a1cc9361fc4308a0b5e5dbd817fb06e72
jungkookswife <146013486+jungkookswife@users.noreply.github.com>
2023-10-05 16:34:30 +0500

    Проверка чисел
    Проверка введённых чисел на соответствие условию задачи

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,3 +2,5 @@ k = int(input('Введите k: '))
 l = int(input('Введите l: '))
 m = int(input('Введите m: '))
 n = int(input('Введите n: '))
+if not(1 <= k <= 8) or not(1 <= l <= 8) or not(1 <= m <= 8) or not(1 <= n <= 8):
+    print("Координаты должны быть натуральными числами от 1 до 8")
